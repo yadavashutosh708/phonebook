329105394c0e284c33e15bb56a6298bb32b19940
Ashutosh <yadavashutosh708@gmail.com>
2024-03-08 00:44:39 +0530

    Reorganising

--- server/src/main/java/com/phonebook/PhonebookApplication.java
+++ server/src/main/java/com/phonebook/PhonebookApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class PhonebookApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PhonebookApplication.class, args);
 	}
